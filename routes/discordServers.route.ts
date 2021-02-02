f7a25509ea63d7602500547408d214d2c26c2825
Mathis Barré <mathis.barre@live.fr>
2021-02-02 11:20:28 +0100

    refacto: add discordServer controller

--- routes/discordServers.route.ts
+++ routes/discordServers.route.ts
@@ -1,39 +1,12 @@
-import express, { Response, Request, Router } from "express"
-import { Document } from "mongoose"
-import DiscordServer, { IdiscordServer } from "../models/discordServer.model"
+import express, { Router } from "express"
+import * as discordServerCtrl from "../controller/discordServer.controller"
 
 const router: Router = express.Router()
 
-router.get("/", (req: Request, res: Response) => {
-  DiscordServer.find()
-    .then((discordServers: Array<IdiscordServer>) => { res.status(200).json(discordServers) })
-    .catch((error: Error) => { res.status(400).json(error) })
-})
-
-router.post("/", (req: Request, res: Response) => {
-  const discordServer: Document = new DiscordServer(req.body)
-
-  discordServer.save()
-    .then(() => { res.status(201).json({ message: "New discord server successfully added", data: req.body })})
-    .catch((error: Error) => { res.status(400).json(error) })
-})
-
-router.get("/:id", (req: Request, res: Response) => {
-  DiscordServer.findById(req.params.id)
-    .then((discordServer: IdiscordServer) => { res.status(200).json(discordServer) })
-    .catch((error: Error) => { res.status(400).json(error) })
-})
-
-router.put("/:id", (req: Request, res: Response) => {
-  DiscordServer.updateOne({ _id: req.params.id }, { _id: req.params.id, ...req.body })
-    .then(() => { res.status(200).json({ message: "Discord server successfully updated", data: req.body })})
-    .catch((error: Error) => { res.status(400).json(error) })
-})
-
-router.delete("/:id", (req: Request, res: Response) => {
-  DiscordServer.deleteOne({ _id: req.params.id })
-    .then(() => { res.status(200).json({ message: "Discord server successfully deleted" })})
-    .catch((error: Error) => { res.status(400).json(error) })
-})
+router.get("/", discordServerCtrl.getAllDiscordServers)
+router.get("/:id", discordServerCtrl.getOneDiscordServer)
+router.post("/", discordServerCtrl.addOneDiscordServer)
+router.put("/:id", discordServerCtrl.updateOneDiscordServer)
+router.delete("/:id", discordServerCtrl.deleteOneDiscordServer)
 
 export default router
