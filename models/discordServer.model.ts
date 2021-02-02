dd95c53fd253d2cce22a55aa718bdc27c48e0209
Mathis Barré <mathis.barre@live.fr>
2021-02-02 15:16:38 +0100

    feat: rename field

--- models/discordServer.model.ts
+++ models/discordServer.model.ts
@@ -2,18 +2,18 @@ import { Schema, model, Model, Document } from 'mongoose'
 
 export interface IdiscordServer extends Document {
   name: string
-  description: string
-  href: string
-  img: string
+  details: string
+  link: string
+  imgUrl: string
   learningPaths?: string[]
   apiUrl?: string
 }
 
 const discordServerSchema: Schema = new Schema({
   name: { type: String, required: true },
-  description: { type: String, required: true },
-  href: { type: String, required: true },
-  img: { type: String, required: true },
+  details: { type: String, required: true },
+  link: { type: String, required: true },
+  imgUrl: { type: String, required: true },
   learningPaths: [{ type: Array, required: false }],
   apiUrl: { type: String, required: false }
 })
